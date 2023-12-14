2f892aadd4c715f5f3ad64f1de0b2d0ff32d06b3
Piper Maru <fourdd4@gmail.com>
2023-12-14 04:00:59 +0300

    fix: auth&api URL

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -90,7 +90,7 @@ class Api {
 }
 
 const api = new Api({
-    url: "http://localhost:3001",
+    url: "https://pipermaru.nomoredomainsmonster.ru",
 });
 
 export default api;
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://pipermaru.nomoredomainsmonster.ru";
 
 export function checkResponse(resp) {
   if (!resp.ok) {
